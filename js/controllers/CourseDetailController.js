1e5fb6ec2db438dfd884b99dfb8221b0d4c53e45
Jianmin Zhou <zhoujianmin@xiaoyezi.com>
2016-06-18 12:52:29 +0800

    Change course ui.
    Signed-off-by: Jianmin Zhou <zhoujianmin@xiaoyezi.com>

--- js/controllers/CourseDetailController.js
+++ js/controllers/CourseDetailController.js
@@ -1,16 +1,21 @@
-app.controller('CourseDetailController', function($rootScope, $scope, $routeParams, courseDetailData, $log) {
+app.controller('CourseDetailController', function($rootScope, $scope, $routeParams, $log, courseDetailData) {
     console.log('$routeParams:id:' + $routeParams.id);
 
     var course = $scope;
 
-    course.courseInfo = {};
+    course.currentCourseIndex = -1;
+    course.courseData = {
+        count: 0
+    }
 
-    course.courseInfo.title = '';
-    course.courseInfo.videoSrc = '';
-    course.courseInfo.desc = '';
-    course.courseInfo.other = '';
-    course.courseInfo.qupu = '';
-    course.courseInfo.tracks = '0';
+    course.courseInfo = {
+        title: '',
+        videoSrc: '',
+        desc: '',
+        other: '',
+        qupu: '',
+        tracks: ''
+    }
 
     courseDetailData.getCourse($routeParams.id)
         .$promise
@@ -25,6 +30,8 @@ app.controller('CourseDetailController', function($rootScope, $scope, $routePara
                 course.courseInfo.other = data.detail[0].other;
                 course.courseInfo.qupu = data.detail[0].qupu_url;
                 course.courseInfo.tracks = data.detail[0].tracks;
+
+                course.currentCourseIndex = 0;
             }
 
             $scope.$broadcast('qupuUpdated', course.courseInfo);
@@ -33,19 +40,47 @@ app.controller('CourseDetailController', function($rootScope, $scope, $routePara
         });
 
     course.isFirstCourse = function() {
-        return false;
+        return (course.currentCourseIndex <= 0);
     }
 
     course.isLastCourse = function() {
-        return false;
+        return (course.currentCourseIndex >= course.courseData.count - 1);
     }
 
-    course.getQupu = function() {
+    course.getCurrentQupu = function() {
         console.log('getqupu:' + course.courseInfo.qupu);
         return course.courseInfo.qupu;
     }
-    course.getTracks= function() {
+
+    course.getCurrentTracks = function() {
         console.log('getTracks:' + course.courseInfo.tracks);
         return course.courseInfo.tracks;
+    }
+
+    course.nextCourse = function() {
+        course.currentCourseIndex++;
+
+        course.updateCurrentCourse();
+    }
+
+    course.prevCourse = function() {
+        course.currentCourseIndex--;
+
+        course.updateCurrentCourse();
+    }
+
+    course.updateCurrentCourse = function() {
+        var i = course.currentCourseIndex ;
+        var data = course.courseData;
+        if (i <= course.courseData.count - 1) {
+                course.courseInfo.title = data.detail[i].title;
+                course.courseInfo.videoSrc = data.detail[i].video_url;
+                course.courseInfo.desc = data.detail[i].info;
+                course.courseInfo.other = data.detail[i].other;
+                course.courseInfo.qupu = data.detail[i].qupu_url;
+                course.courseInfo.tracks = data.detail[i].tracks;
+
+            $scope.$broadcast('qupuUpdated', course.courseInfo);
+        }
     }
 });
